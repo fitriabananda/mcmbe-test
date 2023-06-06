a0a5154c8122d7ac9109e266e81a345772947c73
fitriabananda <13517133@std.stei.itb.ac.id>
2023-06-06 11:23:59 +0700

    setting up sequelize

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const service = require('../services/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  service.connect();
 });
 
 module.exports = router;
--- services/db.js
+++ services/db.js
@@ -1,5 +1,22 @@
 const mysql = require('mysql2/promise');
+const { Sequelize } = require('@sequelize/core');
 const config = require('../config');
+const {host, user, password, database, port} = config.db;
+const sequelize = new Sequelize(database, user, password, {
+    host,
+    port,
+    dialect: 'mysql'
+});
+
+async function connect() {
+    console.log('Checking Sequelize connection..');
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (err) {
+        console.error('Unable to connect to database.', err);
+    }
+}
 
 async function query(sql, params) {
     const connection = await mysql.createConnection(config.db);
@@ -8,5 +25,6 @@ async function query(sql, params) {
 }
 
 module.exports = {
+    connect,
     query
 }
